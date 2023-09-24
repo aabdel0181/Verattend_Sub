204f553c98c3c4e8f39a14c17c6da383362948d0
hfang1732 <76411526+hfang1732@users.noreply.github.com>
2023-09-24 03:02:27 -0400

    pages

--- pages/events.js
+++ pages/events.js
@@ -1,8 +1,19 @@
-import { React } from 'react'
+//import { React } from 'react'
+import React, { useState, useEffect } from 'react';
 import { NavBar } from "../components/navBar"
 import Head from 'next/head'
+import { eventNames, contract } from './_app';
+
 
 function events() {
+    const [names, setNames] = useState([]);
+
+    useEffect(() => {
+        (async function () {
+          const fetchedNames = eventNames;
+          setNames(fetchedNames); 
+        })();
+      }, []);
     return (
         <section className="bg-red-100 min-h-screen">
             <center>
@@ -10,11 +21,11 @@ function events() {
                     <b>All Events</b>
                 </h1>
                 <ul class="w-full text-red-600">
-                    <li className="my-2">Event 1 - <button type="button" class="bg-red-500 text-white px-2 rounded"> Join Event!</button></li>
-                    <li className="my-2">Event 2 - <button type="button" class="bg-red-500 text-white px-2 rounded"> Join Event!</button></li>
-                    <li className="my-2">Event 3 - <button type="button" class="bg-red-500 text-white px-2 rounded"> Join Event!</button></li>
-                    <li className="my-2">Event 4 - <button type="button" class="bg-red-500 text-white px-2 rounded"> Join Event!</button></li>
-                    <li className="my-2">Event 5 - <button type="button" class="bg-red-500 text-white px-2 rounded"> Join Event!</button></li>
+                    {names.map((eventName, i) => (
+                        <li key={i} className="my-2">
+                        {eventName} - <button type="button" className="bg-red-500 text-white px-2 rounded"> Join Event!</button>
+                        </li>
+                    ))}
                 </ul>
             </center>
         </section>
--- pages/organizer.js
+++ pages/organizer.js
@@ -3,6 +3,7 @@ import { React } from 'react'
 import { NavBar } from "../components/navBar"
 import { useEffect, useState } from "react";
 import Head from 'next/head'
+import { contract } from './_app'
 
 //var eventName = document.getElementById();
 
@@ -15,13 +16,13 @@ function pendingApplicants() {
     const checkInBtn = () => {
         window.alert(inputValue);
     };
+    const currentEventName = contract.getEvent.name
     return (
         <section className="bg-red-100 min-h-screen">
 
             <div className="text-center">
                 <h1 className="bg-red-100 text-red-600 textxl py-5 text-4xl">
-                    <b>Manage [Event Name]</b>
-                    {/* Event.name */}
+                    <b>Manage {currentEventName}</b>
                 </h1>
                 <h4 className="textxl py-5 text-2xl">
                     <b>
@@ -29,10 +30,10 @@ function pendingApplicants() {
                         {/* Number(Event.Spots) */}
                     </b>
                 </h4>
-                <div class="columns-2 h-full">
-                    <div class="overflow-scroll">
+                <div className="columns-2 h-full">
+                    <div className="overflow-scroll">
                         <h1><b>Pending Participants</b></h1>
-                        <ul class="w-full">
+                        <ul className="w-full">
                             {/* for (i in Events.participants){
                                 <li>Events.participants[i]</li>
                             } */}
@@ -44,12 +45,12 @@ function pendingApplicants() {
                             <li>Particpant3</li>
                         </ul>
                     </div>
-                    <div class="overflow-scroll">
-                        <h1 class="py-5"><b>Check-In Participants</b></h1>
+                    <div className="overflow-scroll">
+                        <h1 className="py-5"><b>Check-In Participants</b></h1>
                         <input type="text" id="myName" placeholder="Enter Name" value={inputValue}
                             onChange={(e) => setInputValue(e.target.value)}
                         />
-                        <div class="border p-4">
+                        <div className="border p-4">
                             <button type="button" className="bg-red-500 text-white px-4 py-2 rounded" onClick={checkInBtn}>Check-In</button>
 
                         </div>
